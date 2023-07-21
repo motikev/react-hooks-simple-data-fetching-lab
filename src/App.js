1667a0f7898edbb2ad1d80806a13b65159c74e17
motikev <kmotties@gmail.com>
2023-07-21 17:10:35 +0300

    Completed Assignment

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setDogImage(data.message))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
